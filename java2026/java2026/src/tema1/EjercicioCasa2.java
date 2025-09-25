5505427925a14d75a3da21c5ba6056359e73fe4f
jjguillen <jjavierguillen@gmail.com>
2025-09-25 19:00:26 +0200

    Ejemplos

---
+++ java2026/java2026/src/tema1/EjercicioCasa2.java
@@ -0,0 +1,17 @@
+package tema1;
+
+public class EjercicioCasa2 {
+    public static void main(String[] args) {
+
+        //Un conductor de coche quiere saber cuánta gasolina consume en un viaje.
+        //Su coche consume 5.8 litros de gasolina cada 100km
+        //Cuantos litros consume en un viaje de 750km
+
+        //Ahora haz el mismo ejercicio pero pensando que el coche es híbrido y
+        //tiene una batería cargada completamente de 120km
+
+        //Muestra con un operador condicional, si el gasto en litros de gasolina
+        //es mayor en el primer coche o en el segundo
+
+    }
+}
