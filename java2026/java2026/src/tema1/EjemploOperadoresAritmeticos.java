a8b3d93ad0c410bdb46f7adacda9f1be3c1d6509
jjguillen <jjavierguillen@gmail.com>
2025-09-25 18:53:43 +0200

    Ejemplos

--- java2026/java2026/src/tema1/EjemploOperadoresAritmeticos.java
+++ java2026/java2026/src/tema1/EjemploOperadoresAritmeticos.java
@@ -5,6 +5,7 @@ public class EjemploOperadoresAritmeticos {
 
         //long numero1 = 145, numero2 = 23;
         double numero1 = 145, numero2 = 23;
+        double resultado = 0;
 
         System.out.println("Suma = " + (numero1 + numero2) );
         System.out.println("Resta = " + (numero1 - numero2) );
@@ -12,10 +13,8 @@ public class EjemploOperadoresAritmeticos {
         System.out.println("Division = " + (numero1 / numero2) );
         System.out.println("Resto de la división = " + (numero1 % numero2) );
 
-
-
-
-
+        resultado = (numero1 * numero1) + (numero2 / numero1) - (numero2 * 22);
+        System.out.println("Resultado = " + resultado);
 
 
     }
