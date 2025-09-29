a93807f455e2ce4ceaa33a4415258cf444f1a91d
jjguillen <jjavierguillen@gmail.com>
2025-09-29 18:01:30 +0200

    Ejercicio operadores booleanos

--- java2026/java2026/src/tema1/EjercicioOperadoresLogicos.java
+++ java2026/java2026/src/tema1/EjercicioOperadoresLogicos.java
@@ -7,12 +7,18 @@ public class EjercicioOperadoresLogicos {
         int numClasesAsiste = 101;
         int practicasEntregadas = 4;
         boolean autorizacionEspecial = false;
+        boolean resultado;
 
         //Condicion para presentarse a examen final
         //Ha asistido al menos al 80% de las clases Y ha entregado más de 3 prácticas
         //O tiene una autorización especial para presentarse
 
         //Escribe la expresión lógica, piensa el resultado, e imprímelo
+
+        resultado = ( (numClasesAsiste >= (numClasesAnio * 0.8)) && (practicasEntregadas > 3) )
+                    || autorizacionEspecial;
+
+        System.out.println(resultado);
 
 
 
---
+++ java2026/java2026/src/tema1/EjercicioOperadoresLogicos2.java
@@ -0,0 +1,21 @@
+package tema1;
+
+public class EjercicioOperadoresLogicos2 {
+    public static void main(String[] args) {
+
+        //Un cliente recibe un descuento especial si se cumplen estas condiciones:
+        //Es miembro VIP y su compra supera 100€ O
+        //No es VIP pero es su cumpleaños y la compra es mayor a 50€
+        //En ningún caso hay descuento si el cliente tiene pagos pendientes
+
+        boolean clienteVip;
+        double valorCompra;
+        boolean esCumpleaños;
+        boolean tienePagosPendientes;
+
+        //Dar valor a las variables de entrada
+        //Poner la condición
+        //Ver lo que debe dar y comprobar el resultado pintándolo
+
+    }
+}
