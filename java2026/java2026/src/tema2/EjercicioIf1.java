6b376aee1d0f7c1429d366addae8b019b37506f0
jjguillen <jjavierguillen@gmail.com>
2025-10-07 17:43:45 +0200

    Ejercicios repaso

---
+++ java2026/java2026/src/tema2/EjercicioIf1.java
@@ -0,0 +1,20 @@
+package tema2;
+
+public class EjercicioIf1 {
+    public static void main(String[] args) {
+
+        //Determinar si eres elegible para que te den un crédito
+        //1. Tienes más de 5 años de antiguedad en el banco e ingresos mensuales de
+        //   más de 2500€
+        //2. Tienes una antigüedad entre 2 y 5 años, ingresos de más de 2000€ mensuales
+        //   y no tienes deudas pendientes
+        //3. Tienes menos de 2 años de antiguedad, un ingreso de más de 3000€ mensuales
+        //   y no tienes deudas pendientes
+
+        //V.E.: antiguedad, ingresosMensuales, deudadPendentes
+        //V.S.: decir si eres elegible para que te den un crédito o no
+
+
+
+    }
+}
