9bf1de34f1d939c079746bf9f51872234ceda292
jjguillen <jjavierguillen@gmail.com>
2025-10-07 17:23:58 +0200

    Ejercicios repaso

---
+++ java2026/java2026/src/tema2/EjemploIfElseAnidados.java
@@ -0,0 +1,22 @@
+package tema2;
+
+public class EjemploIfElseAnidados {
+    public static void main(String[] args) {
+
+        int edad = 17;
+        char sexo = 'h'; //'h', 'm'  if (sexo == 'h')
+        boolean amigoJefe = false;
+
+        //Puedes entrar a la discoteca si tienes 18 o más años
+        //Puedes entrar si tienes más de dieciséis y eres mujer (no deberías)
+        //Puedes entrar si tienes más de dieciséis y hombre y amigo del jefe
+        //No puedes entrar en otro caso
+
+
+
+
+
+
+
+    }
+}
