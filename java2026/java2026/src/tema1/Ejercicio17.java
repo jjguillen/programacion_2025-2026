c9db6502b8f9f55873cf809d1d3590dde791e4e6
jjguillen <jjavierguillen@gmail.com>
2025-10-09 17:11:57 +0200

    Ejercicios repaso

--- java2026/java2026/src/tema1/Ejercicio17.java
+++ java2026/java2026/src/tema1/Ejercicio17.java
@@ -3,6 +3,9 @@ package tema1;
 public class Ejercicio17 {
     public static void main(String[] args) {
 
+        int numero=2, cuad=numero * numero;
+        System.out.println("Cuad: " + cuad);
+
         //int n = (int) (Math.random() * (<número_máximo + 1> - <número_mínimo>))
         // + <numero_mínimo>;
 
@@ -27,6 +30,46 @@ public class Ejercicio17 {
             System.out.println(77);
         if (n==5)
             System.out.println(125);
+
+        //g. Meses del año --> 12
+        n = (int) (Math.random() * 12) + 1;
+        System.out.println(n);
+        if (n == 1) {
+            System.out.println("Enero");
+        }
+        if (n == 2) {
+            System.out.println("Febrero");
+        }
+        if (n == 3) {
+            System.out.println("Marzo");
+        }
+        if (n == 4) {
+            System.out.println("Abril");
+        }
+        if (n == 5) {
+            System.out.println("Mayo");
+        }
+        if (n == 6) {
+            System.out.println("Junio");
+        }
+        if (n == 7) {
+            System.out.println("Julio");
+        }
+        if (n == 8) {
+            System.out.println("Agosto");
+        }
+        if (n == 9) {
+            System.out.println("Septiembre");
+        }
+        if (n == 10) {
+            System.out.println("Octubre");
+        }
+        if (n == 11) {
+            System.out.println("Noviembre");
+        }
+        if (n == 12) {
+            System.out.println("Diciembre");
+        }
 
 
 
