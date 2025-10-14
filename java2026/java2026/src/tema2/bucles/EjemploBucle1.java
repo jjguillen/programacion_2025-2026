48c03746c231a2987fdfc11086df108252124937
jjguillen <jjavierguillen@gmail.com>
2025-10-14 16:57:23 +0200

    Bucles

--- java2026/java2026/src/tema2/bucles/EjemploBucle1.java
+++ java2026/java2026/src/tema2/bucles/EjemploBucle1.java
@@ -6,7 +6,21 @@ public class EjemploBucle1 {
         int contador = 0;
         while (contador <= 10) {
             System.out.println(contador);
-            contador++;
+            contador++; //Acercarnos a que se cumpla la condición de fin del bucle
+        }
+
+        //Saca en un bucle los números del -10 al 10
+        contador = -10;
+        while (contador <= 10) {
+            System.out.println(contador);
+            contador++; //Acercarnos a que se cumpla la condición de fin del bucle
+        }
+
+        //Cuenta atrás del 10 al 1
+        contador = 10;
+        while (contador >= 1) {
+            System.out.println(contador);
+            contador--;
         }
 
         System.out.println("Final del programa");
