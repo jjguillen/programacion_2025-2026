884eafd792947d3df33fbc26d6ee62dd9514e316
jjguillen <jjavierguillen@gmail.com>
2025-10-14 18:24:04 +0200

    Bucles

---
+++ java2026/java2026/src/tema2/bucles/EjercicioBucles3.java
@@ -0,0 +1,17 @@
+package tema2.bucles;
+
+public class EjercicioBucles3 {
+    public static void main(String[] args) {
+
+        //Adivinar un número generado por el pc entre (1-100)
+        //Pedir por teclado un número entre 1 y 100, y el program
+        //nos responde si el número introducido es igual (fin), mayor
+        //o menor que el número generado a acertar.
+        //Extra: 7 intentos máximo
+
+        int numeroAAdivinar =  (int) (Math.random() * 100) + 1;
+
+
+
+    }
+}
