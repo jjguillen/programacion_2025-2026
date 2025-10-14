7abca5bd17dd25891205da76392973843ef19d44
jjguillen <jjavierguillen@gmail.com>
2025-10-14 16:40:45 +0200

    Cambio de paquetes condicionales y bucles

---
+++ java2026/java2026/src/tema2/bucles/EjemploBucle1.java
@@ -0,0 +1,16 @@
+package tema2.bucles;
+
+public class EjemploBucle1 {
+    public static void main(String[] args) {
+
+        int contador = 0;
+        while (contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+
+        System.out.println("Final del programa");
+
+
+    }
+}
