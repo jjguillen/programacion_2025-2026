ea380b3198b63cf5cf6fdced56bcaa9e6c47093c
jjguillen <jjavierguillen@gmail.com>
2025-10-14 18:10:35 +0200

    Bucles

---
+++ java2026/java2026/src/tema2/bucles/EjercicioBucles2.java
@@ -0,0 +1,13 @@
+package tema2.bucles;
+
+public class EjercicioBucles2 {
+    public static void main(String[] args) {
+        //Lanza dos dados (1-6) mientras sean diferentes
+        //Cuenta las veces que has tenido que tirar los dados
+
+
+
+
+
+    }
+}
