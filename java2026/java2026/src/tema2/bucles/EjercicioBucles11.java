8a5349505b12caede3b33693d89746a61caf0add
jjguillen <jjavierguillen@gmail.com>
2025-10-16 18:37:46 +0200

    Bucles

---
+++ java2026/java2026/src/tema2/bucles/EjercicioBucles11.java
@@ -0,0 +1,22 @@
+package tema2.bucles;
+
+public class EjercicioBucles11 {
+    public static void main(String[] args) {
+
+        //Hay dos romanos Cubitus y Humerus en la frontera germana aburridos
+        //Juegan a los dados y se determina quien gana.
+        //Cada jugador tiene 2 dados de seis caras
+        //Gana el jugador que sume más sus dos dados
+        //Juegan 10 rondas
+        //Quién gana más rondas?
+
+        //V.E.: tiradaC1, tiradaC2, tiradaH1, tiradaH2
+        //      sumaC, sumaH
+        //      contRondasGC, contRondasGH
+
+
+
+
+
+    }
+}
