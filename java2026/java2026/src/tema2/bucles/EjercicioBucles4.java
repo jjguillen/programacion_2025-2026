93b7f022faeb737a8c8b4cf71e6d0d38f871b533
jjguillen <jjavierguillen@gmail.com>
2025-10-16 16:22:24 +0200

    Bucles

--- java2026/java2026/src/tema2/bucles/EjercicioBucles4.java
+++ java2026/java2026/src/tema2/bucles/EjercicioBucles4.java
@@ -11,21 +11,28 @@ public class EjercicioBucles4 {
 
         System.out.println("Dime primer número (1-100): ");
         num1 = sc.nextInt();
-        System.out.println("Dime segundo número (1-100): ");
-        num2 = sc.nextInt();
 
-        //Intercambiamos si num1 es mayor que num2
-        if (num1 > num2) {
+        //Asegurarme que num2 es mayor que num1
+        do {
+            System.out.println("Dime segundo número mayor que el anterior: ");
+            num2 = sc.nextInt();
+        } while (num2 <= num1);
+
+        /*
+        //O asegurarme intercambiando valores si num2 es menor que num1
+        if (num2 < num1) {
             int temp = num1;
             num1 = num2;
             num2 = temp;
         }
+        */
 
-        while (num1 <= num2) {
-            if (num1 % 2 == 0) {
-                System.out.println(num1);
+        int contador = num1;
+        while (contador <= num2) {
+            if (contador % 2 == 0) {
+                System.out.println(contador);
             }
-            num1++;
+            contador++;
         }
 
     }
