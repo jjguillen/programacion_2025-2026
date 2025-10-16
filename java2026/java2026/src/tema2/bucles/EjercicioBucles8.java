87d6c31ca0664905a70e4db780871bb9f05acb94
jjguillen <jjavierguillen@gmail.com>
2025-10-16 17:47:06 +0200

    Bucles

---
+++ java2026/java2026/src/tema2/bucles/EjercicioBucles8.java
@@ -0,0 +1,21 @@
+package tema2.bucles;
+
+import java.util.Scanner;
+
+public class EjercicioBucles8 {
+    public static void main(String[] args) {
+
+        //Pide 10 palabras por teclado y dime cuántas de ellas
+        //tienen longitud mayor de 5 caracteres
+        //cadena.length() devuelve la longitud de la cadena
+
+        String palabra = "Hola";
+        System.out.println(palabra.length());
+
+        Scanner sc = new Scanner(System.in);
+        int contador = 0;
+
+
+
+    }
+}
