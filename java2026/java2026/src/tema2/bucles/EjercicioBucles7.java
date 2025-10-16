ce0975ba324e9916387998f46fe2592225f4debf
jjguillen <jjavierguillen@gmail.com>
2025-10-16 17:21:07 +0200

    Bucles

---
+++ java2026/java2026/src/tema2/bucles/EjercicioBucles7.java
@@ -0,0 +1,12 @@
+package tema2.bucles;
+
+public class EjercicioBucles7 {
+    public static void main(String[] args) {
+
+        //Números que sean múltiplos de 3 pero no de 5 entre 100 y 200
+        int contador = 0;
+
+
+
+    }
+}
