3ad7e39f158e4d6620560e1c36cd644f3f1f59c2
jjguillen <jjavierguillen@gmail.com>
2025-10-20 17:37:22 +0200

    Bucles

---
+++ java2026/java2026/src/tema2/bucles/BucleAcumulador4.java
@@ -0,0 +1,23 @@
+package tema2.bucles;
+
+public class BucleAcumulador4 {
+    public static void main(String[] args) {
+
+        //Humerus y Cubitus se aburren el finde y van a jugar a los dados
+        //Humerus tira tres dados y su puntuación es la suma de los pares
+        //Cubitus tira tres dados y su puntuación es la suma de los impares
+        //Si jugan 10 rondas, dime el ganador
+
+        // dado = (int) (Math.random() * 6 + 1);
+
+        //V.E.: tiradaDH1, tiradaDH2, tiradaDH3, tiradaDC1, tiradaDC2, tiradaDC3
+        //      sumaDH, sumaDC
+        //      contaRondasH, contaRondasC
+        // Juegan 10 rondas
+        //V.S.: Si contaRondasH > contaRondasC --> Gana Humerus
+        //      Si contaRondasC > contaRondasH --> Gana Cubitus
+        //      Si son iguales --> Empate
+
+
+    }
+}
