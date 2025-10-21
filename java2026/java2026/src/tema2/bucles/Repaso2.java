e01fc9454387561dab78faec0bc3c6c3ddd6c85b
jjguillen <jjavierguillen@gmail.com>
2025-10-21 18:47:26 +0200

    Bucles

--- java2026/java2026/src/tema2/bucles/Repaso2.java
+++ java2026/java2026/src/tema2/bucles/Repaso2.java
@@ -1,5 +1,7 @@
 package tema2.bucles;
 
+import java.util.Scanner;
+
 public class Repaso2 {
     public static void main(String[] args) {
         // Realizar un algoritmo que pida números (se pedirá por teclado la
@@ -7,6 +9,32 @@ public class Repaso2 {
         // cuantos números introducidos son mayores que 0,
         // menores que 0 (e iguales a 0)
 
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        int num;
+        int contadorMayores=0;
+        int contadorMenores=0;
+        int contadorIguales=0;
+
+        System.out.println("Dime cuántos números vas a introducir: ");
+        numero = sc.nextInt();
+
+        for(int i=0; i < numero; i++) {
+            System.out.println("Introduce número: ");
+            num = sc.nextInt();
+
+            if(num > 0) {
+                contadorMayores++;
+            } else if(num < 0) {
+                contadorMenores++;
+            } else {
+                contadorIguales++;
+            }
+        }
+
+        System.out.println("Mayores: " + contadorMayores);
+        System.out.println("Menores: " + contadorMenores);
+        System.out.println("Iguales: " + contadorIguales);
 
 
     }
