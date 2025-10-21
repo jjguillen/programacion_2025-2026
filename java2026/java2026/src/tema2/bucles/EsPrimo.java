dfa1f2b944d382609eedaea546d1cca33185be24
jjguillen <jjavierguillen@gmail.com>
2025-10-21 17:59:58 +0200

    Bucles

---
+++ java2026/java2026/src/tema2/bucles/EsPrimo.java
@@ -0,0 +1,18 @@
+package tema2.bucles;
+
+public class EsPrimo {
+    public static void main(String[] args) {
+
+        //Pide un número por teclado y comprueba si es o no primo
+        //Un número es primo si SOLO es divisible por 1 y por el mismo
+        //Ejemplo: 19
+
+        //Modo de hacerlo:
+        //Indicador esPrimo=true
+        //Bucle desde el 2 hasta el número - 1
+        //Si el número es divisible por alguno de esos, esPrimo=false
+
+
+
+    }
+}
