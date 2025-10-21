862e5cd55fce4d0e0afc5d37fdeb0c2656d18b64
jjguillen <jjavierguillen@gmail.com>
2025-10-21 16:44:59 +0200

    Bucles

--- java2026/java2026/src/tema2/bucles/BucleIndicador2.java
+++ java2026/java2026/src/tema2/bucles/BucleIndicador2.java
@@ -1,5 +1,7 @@
 package tema2.bucles;
 
+import java.util.Scanner;
+
 public class BucleIndicador2 {
     public static void main(String[] args) {
 
@@ -7,7 +9,23 @@ public class BucleIndicador2 {
         //tiene una longitud mayor de 5 caracteres
         //Pista: cadena.length() devuelve la longitud de la cadena
 
+        Scanner sc = new Scanner(System.in);
+        String palabra;
+        boolean mayorDe5 = false; //Indicador
+        for(int i=0; i < 5; i++) {
+            System.out.println("Dime una palabra: ");
+            palabra = sc.nextLine();
 
+            if (palabra.length() > 5) {
+                mayorDe5 = true;
+            }
+        }
+
+        if (mayorDe5) {
+            System.out.println("Alguna de las palabras es mayor de 5 caracteres");
+        } else {
+            System.out.println("Ninguna de las palabras es mayor de 5 caracteres");
+        }
 
 
 
