a56a6743c0015cc7f86454777f851ecdab6e27af
jjguillen <jjavierguillen@gmail.com>
2025-10-21 18:31:06 +0200

    Bucles

---
+++ java2026/java2026/src/tema2/bucles/Repaso2.java
@@ -0,0 +1,10 @@
+package tema2.bucles;
+
+public class Repaso2 {
+    public static void main(String[] args) {
+        // Realizar un algoritmo que pida números (se pedirá por teclado la
+        // cantidad de números a introducir). El programa debe informar de
+        // cuantos números introducidos son mayores que 0,
+        // menores que 0 e iguales a 0
+    }
+}
