074dcdf87eed94281d446b3508700d3a877a469d
jjguillen <jjavierguillen@gmail.com>
2025-10-21 17:11:54 +0200

    Bucles

---
+++ java2026/java2026/src/tema2/bucles/Media.java
@@ -0,0 +1,19 @@
+package tema2.bucles;
+
+public class Media {
+    public static void main(String[] args) {
+
+        // Programa que pida números hasta que se introduzca un cero.
+        // Debe imprimir la suma y la media de todos los números introducidos.
+
+        //1. Bucle infinito
+        //2. Ir pidiendo por teclado cada iteración un número
+        //3. Cuando el número sea igual a 0 -> termina el bucle (break)
+        //4. En un acumulador guardar la suma de todos los números
+        //5. Un contador (i) para saber cuántos números pido por teclado
+        //6. Media = acumulador / contador
+
+
+
+    }
+}
