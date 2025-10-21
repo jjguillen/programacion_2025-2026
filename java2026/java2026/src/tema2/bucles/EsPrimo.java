2c401f3f1d0c039f2ab32ef1df352e7cd25c9e3c
jjguillen <jjavierguillen@gmail.com>
2025-10-21 18:26:50 +0200

    Bucles

--- java2026/java2026/src/tema2/bucles/EsPrimo.java
+++ java2026/java2026/src/tema2/bucles/EsPrimo.java
@@ -1,5 +1,7 @@
 package tema2.bucles;
 
+import java.util.Scanner;
+
 public class EsPrimo {
     public static void main(String[] args) {
 
@@ -12,7 +14,25 @@ public class EsPrimo {
         //Bucle desde el 2 hasta el número - 1
         //Si el número es divisible por alguno de esos, esPrimo=false
 
+        Scanner sc = new Scanner(System.in);
+        int numero;
 
+        System.out.println("Dime un número entero: ");
+        numero = sc.nextInt();
+        boolean esPrimo = true;
+        for(int i=2; i < numero; i++) {
+            //Encontrar un divisor
+            if (numero % i == 0) {
+                esPrimo = false;
+                break; //Si encontramos ya no es primo y no tiene sentido seguir buscando
+            }
+        }
+
+        if(esPrimo) {
+            System.out.println("Es primo");
+        } else {
+            System.out.println("No es primo");
+        }
 
     }
 }
