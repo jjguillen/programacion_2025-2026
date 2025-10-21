132a2b7d3d4b1bbd303664873146d2d2a7642721
jjguillen <jjavierguillen@gmail.com>
2025-10-21 16:35:25 +0200

    Bucles

---
+++ java2026/java2026/src/tema2/bucles/BucleIndicador2.java
@@ -0,0 +1,16 @@
+package tema2.bucles;
+
+public class BucleIndicador2 {
+    public static void main(String[] args) {
+
+        //Pide 5 cadenas de texto por teclado y dime si alguna de ellas
+        //tiene una longitud mayor de 5 caracteres
+        //Pista: cadena.length() devuelve la longitud de la cadena
+
+
+
+
+
+
+    }
+}
