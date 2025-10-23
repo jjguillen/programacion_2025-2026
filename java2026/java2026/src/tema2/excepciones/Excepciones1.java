666bc359357e6fef0b8654bba033d9eeb89170c3
jjguillen <jjavierguillen@gmail.com>
2025-10-23 18:25:22 +0200

    Bucles

---
+++ java2026/java2026/src/tema2/excepciones/Excepciones1.java
@@ -0,0 +1,28 @@
+package tema2.excepciones;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Excepciones1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int opcion=-1;
+
+        try {
+            System.out.println("Dime un número");
+            opcion = sc.nextInt();
+        } catch (InputMismatchException exc) { //Si no es entero
+            System.out.println("Por favor introduce un entero");
+        } catch (Exception exc) { //Cualquier error capturado
+            System.out.println("Error leyendo de teclado");
+        }
+
+        //SOLO CAPTURA UNA EXCEPCION: la más específica
+        //Orden de los catchs es de excepción más concreta a excepción
+        //más general. La más general es Exception, y se pone al final.
+
+        System.out.println("FIN DEL PROGRAMA");
+
+    }
+}
---
+++ java2026/java2026/src/tema2/procedimientosYFunciones/Ejercicio1.java
@@ -0,0 +1,7 @@
+package tema2.procedimientosYFunciones;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+    }
+}
