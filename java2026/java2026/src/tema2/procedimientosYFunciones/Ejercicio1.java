52a8da00db36ca61afb1d9440b36b1e0e3f2f941
jjguillen <jjavierguillen@gmail.com>
2025-10-23 18:26:59 +0200

    Bucles

--- java2026/java2026/src/tema2/procedimientosYFunciones/Ejercicio1.java
+++ java2026/java2026/src/tema2/procedimientosYFunciones/Ejercicio1.java
@@ -3,5 +3,16 @@ package tema2.procedimientosYFunciones;
 public class Ejercicio1 {
     public static void main(String[] args) {
 
+        //Crea una función que genere un número aleatorio entre
+        //un número entero mínimo y un máximo
+        //generarAleatorio(int min, int max)
+        //Pruébala desde el main llamándola con:
+        //generarAleatorio(1, 100)
+        //generarAleatorio(-100, 100)
+        //generarAleatorio(100, 200)
+
+
+
+
     }
 }
