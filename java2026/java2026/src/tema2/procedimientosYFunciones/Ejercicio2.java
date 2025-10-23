456c9cc7d20146fd8864b5ab91c5e026d6ac6932
jjguillen <jjavierguillen@gmail.com>
2025-10-23 18:45:31 +0200

    Funciones y procedimientos

--- java2026/java2026/src/tema2/procedimientosYFunciones/Ejercicio2.java
+++ java2026/java2026/src/tema2/procedimientosYFunciones/Ejercicio2.java
@@ -1,19 +1,19 @@
 package tema2.procedimientosYFunciones;
 
 public class Ejercicio2 {
+
+
     public static void main(String[] args) {
         //Crear un procedimiento que pinte en la misma línea
         //tantos asteriscos como indique un parámetro llamado num
+        //Ejemplo: pintarAsteriscos(5) --> *****
         //Al final que pinte un salto de línea
-
 
         pintarAsteriscos(5);
         pintarAsteriscos(4);
         pintarAsteriscos(3);
         pintarAsteriscos(2);
         pintarAsteriscos(1);
-
-
 
     }
 }
