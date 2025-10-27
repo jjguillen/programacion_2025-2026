a2af98a330659adb2971d8a30252bcb36fd5e174
jjguillen <jjavierguillen@gmail.com>
2025-10-27 17:44:54 +0100

    Funciones y procedimientos

--- java2026/java2026/src/tema2/procedimientosYFunciones/MultiplesAreas.java
+++ java2026/java2026/src/tema2/procedimientosYFunciones/MultiplesAreas.java
@@ -11,8 +11,7 @@ public class MultiplesAreas {
         System.out.println("4. Área de un triángulo");
         System.out.println("5. Área de un rombo"); //(diagonalM * diagonalm) / 2
         System.out.println("6. Área de un hexágono"); //(lado * 6 * radio) / 2
-
-        System.out.println("5. Salir");
+        System.out.println("7. Salir");
     }
 
     public static double area(double valor1, double valor2, String tipo) {
@@ -25,6 +24,10 @@ public class MultiplesAreas {
                 return calcAreaRectangulo(valor1, valor2);
             case "Triangulo":
                 return Funciones3.areaTriangulo(valor1, valor2);
+            case "Rombo":
+                return calcAreaRombo(valor1, valor2);
+            case "Hexagono":
+                return calcAreaHexagono(valor1, valor2);
             default:
                 return 0;
         }
@@ -42,13 +45,21 @@ public class MultiplesAreas {
         return base * altura;
     }
 
+    public static double calcAreaRombo(double diagonalM, double diagonalm) {
+        return (diagonalM * diagonalm) / 2;
+    }
+
+    public static double calcAreaHexagono(double lado, double radio)  {
+        return (lado * 6 * radio) / 2;
+    }
+
 
     public static void main(String[] args) {
         //Menú para decir qué tipo de forma para calcular el área quieres
 
         Scanner sc = new Scanner(System.in);
         int opcion=-1;
-        double radio, lado, base, altura;
+        double radio, lado, base, altura, diagonalM, diagonalm;
 
         do {
             try {
@@ -86,6 +97,23 @@ public class MultiplesAreas {
                                 + area(base, altura, "Triangulo"));
                         break;
                     case 5:
+                        System.out.println("Dime diagonal mayor: ");
+                        diagonalM = sc.nextDouble();
+                        System.out.println("Dime diagonal menor: ");
+                        diagonalm = sc.nextDouble();
+                        System.out.println("El área es "
+                                + area(diagonalM, diagonalm, "Rombo"));
+                        break;
+                    case 6:
+                        System.out.println("Dime lado: ");
+                        lado = sc.nextDouble();
+                        System.out.println("Dime radio: ");
+                        radio = sc.nextDouble();
+                        System.out.println("El área es "
+                                + area(lado, radio, "Hexagono"));
+                        break;
+
+                    case 7:
                         System.out.println("Gracias por usar nuestro programa");
                         break;
                     default:
@@ -98,7 +126,7 @@ public class MultiplesAreas {
                 sc = new Scanner(System.in);
             }
 
-        } while(opcion != 5);
+        } while(opcion != 7);
 
 
     }
