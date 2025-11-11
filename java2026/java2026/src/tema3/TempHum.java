61328f0a03f38bb24bea2c5059235a6f194fa593
jjguillen <jjavierguillen@gmail.com>
2025-11-11 18:15:26 +0100

    Prueba clase, Opción 1

---
+++ java2026/java2026/src/tema3/TempHum.java
@@ -0,0 +1,22 @@
+package tema3;
+
+public class TempHum {
+
+    public static void main(String[] args) {
+
+        //Rellenar un array de 100 elementos de tipo double con valores
+        //aleatorios de temperatura entre -20 y 40
+
+        //Rellenar un array de 100 elementos de tipo double con valores
+        //aleatorios de humedad entre 0 y 100
+
+        //Haz un programa que muestre por pantalla la temperatura y la humedad
+        //media conforme vas generando valores aleatorios
+
+        //Si la temperatura media es superior a 30 grados en algún momento debe
+        //parar el bucle y mostrar un mensaje indicando que ha superado la temperatura
+        //media permitida.
+
+
+    }
+}
