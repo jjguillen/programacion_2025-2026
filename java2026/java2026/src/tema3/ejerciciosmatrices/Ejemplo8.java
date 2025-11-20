84c0eb7709cff65d8aa2f47eea40b2d0117125ff
jjguillen <jjavierguillen@gmail.com>
2025-11-20 18:07:47 +0100

    Ejercicios repaso arrays

---
+++ java2026/java2026/src/tema3/ejerciciosmatrices/Ejemplo8.java
@@ -0,0 +1,14 @@
+package tema3.ejerciciosmatrices;
+
+public class Ejemplo8 {
+    public static void main(String[] args) {
+
+        /**
+         * Dada una matriz 8x8 que representa una tablero de ajedrez
+         * Dadas las coordenadas i,j donde está el rey en el tablero
+         * Dime a qué posiciones del tablero se puede mover el rey
+         */
+
+
+    }
+}
