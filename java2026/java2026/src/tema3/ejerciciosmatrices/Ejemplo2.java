6359f191e42ab1d8882b1acc2300e2ecca1c8853
jjguillen <jjavierguillen@gmail.com>
2025-11-20 16:53:09 +0100

    Ejercicios repaso arrays

--- java2026/java2026/src/tema3/ejerciciosmatrices/Ejemplo2.java
+++ java2026/java2026/src/tema3/ejerciciosmatrices/Ejemplo2.java
@@ -2,13 +2,49 @@ package tema3.ejerciciosmatrices;
 
 public class Ejemplo2 {
 
-
-    public static void pintarMatriz(double[][] m) {
+    /**
+     * Máximo de una matriz de doubles
+     * Tomamos el máximo como el primer elemento y vamos recorriendo la matriz.
+     * Si algún elemento de la matriz es mayor actualizamos el valor
+     * máximo a ese elemento.
+     * @param m
+     * @return
+     */
+    public static double maximo(double[][] m) {
+        double maximo = m[0][0];
         for(int i=0; i < m.length; i++) {
             for(int j=0; j < m[i].length; j++) {
-                System.out.printf("%5.2f ", m[i][j]);
+                if (m[i][j] > maximo) {
+                    maximo = m[i][j];
+                }
             }
-            System.out.println();  // 3.14 //Pinto el salto de línea al terminar la fila
+        }
+
+        return maximo;
+    }
+
+    public static void pintarLinea(double[][] m) {
+        for(int i=0; i < m[0].length; i++){
+            System.out.print("+");
+            for(int j=0; j < m.length+4; j++){
+                System.out.print("-");
+            }
+        }
+        System.out.println("+");
+    }
+
+    public static void pintarMatriz(double[][] m) {
+
+        pintarLinea(m);
+        for (int i = 0; i < m.length; i++) {
+            System.out.print("|");
+
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.printf(" %6.2f |", m[i][j]);  // 2 decimales, ancho fijo
+            }
+
+            System.out.println();
+            pintarLinea(m);
         }
     }
 
@@ -17,7 +53,7 @@ public class Ejemplo2 {
         //Rellenar matriz de doubles 10x10 con números aleatorios entre 0.0 y 1.0
         //Encontrar el valor máximo y mínimo de la matriz
 
-        double[][] matriz = new double[10][10];
+        double[][] matriz = new double[4][4];
 
         for(int i=0; i < matriz.length; i++) {
             for(int j=0; j < matriz[i].length; j++) {
