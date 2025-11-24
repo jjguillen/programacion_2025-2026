bbf53e73ce3e785d3ff39042c10f9e821c018ed4
jjguillen <jjavierguillen@gmail.com>
2025-11-24 17:43:10 +0100

    Ejercicios repaso arrays

--- java2026/java2026/src/tema3/ejerciciosmatrices/CazaTesoro.java
+++ java2026/java2026/src/tema3/ejerciciosmatrices/CazaTesoro.java
@@ -1,5 +1,8 @@
 package tema3.ejerciciosmatrices;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class CazaTesoro {
     /**
      * Escribe un programa en Java que simule un juego de la caza del tesoro. En
@@ -32,6 +35,21 @@ public class CazaTesoro {
      */
 
 
+    public static void pintarMatriz(char[][] m) {
+        for(int i=0; i < m.length; i++) {
+            for(int j=0; j < m[i].length; j++) {
+                System.out.printf("%2c", m[i][j]);
+            }
+            System.out.println();  // 3.14 //Pinto el salto de línea al terminar la fila
+        }
+    }
+
+    public static int distanciaManhattan(int disparoX, int disparoY,
+                                         int tesoroX, int tesoroY) {
+        return Math.abs(disparoX - tesoroX) + Math.abs(disparoY - tesoroY);
+    }
+
+
     public static void main(String[] args) {
 
         /**
@@ -51,6 +69,73 @@ public class CazaTesoro {
          *
          */
 
+        Scanner sc = new Scanner(System.in);
+
+        char[][] tablero = new char[10][10];
+        int tesoroX = 0, tesoroY = 0;
+        int disparoX = 0, disparoY = 0;
+        int distanciaAnterior = 20; //Mayor que la mayor distancia posible
+        int distanciaActual = 0;
+        int turnos = 15;
+
+        for(int i=0; i < tablero.length; i++) {
+            for(int j=0; j < tablero[i].length; j++) {
+                tablero[i][j] = '-';
+            }
+        }
+
+        //pintarMatriz(tablero);
+        tesoroX = (int) (Math.random() * 10);
+        tesoroY = (int) (Math.random() * 10);
+
+        tablero[tesoroX][tesoroY] = 'T';
+        pintarMatriz(tablero);
+
+        //Bucle hasta agotar turnos
+        do {
+
+            do {
+                System.out.println("Dime posicion X de (X,y) (0-9):");
+                disparoX = Integer.parseInt(sc.nextLine());
+            } while (disparoX < 0 || disparoX > 9);
+            do {
+                System.out.println("Dime posicion Y de (x,Y) (0-9):");
+                disparoY = Integer.parseInt(sc.nextLine());
+            } while (disparoY < 0 || disparoY > 9);
+
+            tablero[disparoX][disparoY] = '*';
+            pintarMatriz(tablero);
+
+            distanciaActual = distanciaManhattan(disparoX, disparoY, tesoroX, tesoroY);
+            System.out.println("Distancia del disparo: " + distanciaActual);
+
+            //Determinar si estás más cerca o más lejos del tesoro
+            if (distanciaActual < distanciaAnterior) {
+                System.out.println("Estás más cerca del tesoro");
+            } else if (distanciaActual > distanciaAnterior) {
+                System.out.println("Estás más lejos del tesoro");
+            } else {
+                System.out.println("Estás igual de lejos o cerca del tesoro");
+            }
+
+            //Actualizar distancia anterior
+            distanciaAnterior = distanciaActual;
+
+            //Condición de fin
+            if (distanciaActual == 0) {
+                System.out.println("Has encontrado el tesoro!");
+                break;
+            }
+
+            //Restamos un turno
+            turnos--;
+            System.out.println("Te quedan " + turnos + " turnos");
+        } while(turnos > 0);
+
+        //Si sales del while por agotar los turnos has perdido
+        if (turnos == 0) {
+            System.out.println("Has perdido");
+        }
 
     }
 }
