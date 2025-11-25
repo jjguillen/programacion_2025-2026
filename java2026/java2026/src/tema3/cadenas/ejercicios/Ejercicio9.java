599d8ed46cca29b68537200e1a9bdb7386c4cb4d
jjguillen <jjavierguillen@gmail.com>
2025-11-25 18:31:19 +0100

    Ejercicios cadenas

---
+++ java2026/java2026/src/tema3/cadenas/ejercicios/Ejercicio9.java
@@ -0,0 +1,19 @@
+package tema3.cadenas.ejercicios;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+
+        /**
+         * Crea un programa en Java que solicite al usuario dos cadenas de caracteres
+         * y que devuelva la primera cadena, pero transformando en mayúsculas la parte
+         * que coincide con la segunda cadena introducida.
+         * Por ejemplo, si se introducen las cadenas "Esta es mi amiga Ana" y
+         * "amiga" devolverá "Esta es mi AMIGA Ana".
+         *
+         * replace o replaceAll
+         */
+
+
+
+    }
+}
