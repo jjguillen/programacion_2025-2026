741b00df7b81031f70d9ad96234f0590fcb27c43
jjguillen <jjavierguillen@gmail.com>
2025-11-25 18:11:50 +0100

    Ejercicios cadenas

---
+++ java2026/java2026/src/tema3/cadenas/ejercicios/Ejercicio7.java
@@ -0,0 +1,26 @@
+package tema3.cadenas.ejercicios;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+
+        /**
+         * Diseña un programa en Java que solicite al usuario una cadena
+         * en la que buscará y otra que será la cadena buscada. El programa
+         * indicará cuántas veces aparece la segunda cadena en la primera.
+         *
+         * cadena = "El día de hoy es el más largo de mi vida."
+         * subcadena = "de"
+         * contador = 2
+         *
+         * indexOf
+         *
+         */
+
+        String cadena = "El día de hoy es el más largo de mi vida.";
+        String subcadena = "de";
+        int contador = 0;
+
+
+
+    }
+}
