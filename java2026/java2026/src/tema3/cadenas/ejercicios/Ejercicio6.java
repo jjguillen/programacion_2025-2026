9fe2ddc8ef6e54a7e0ec1ec8847edb6018968c6a
jjguillen <jjavierguillen@gmail.com>
2025-11-25 17:56:43 +0100

    Ejercicios cadenas

---
+++ java2026/java2026/src/tema3/cadenas/ejercicios/Ejercicio6.java
@@ -0,0 +1,14 @@
+package tema3.cadenas.ejercicios;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+
+        /**
+         * Diseña un programa en Java que solicite al usuario una cadena de
+         * caracteres y muestre por pantalla un conteo de cuántas vocales,
+         * consonantes y espacios en blanco contiene la cadena introducida.
+         */
+
+
+    }
+}
