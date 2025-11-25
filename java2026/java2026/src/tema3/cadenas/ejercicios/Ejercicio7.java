574267e2216366086c681615ff663cf099191cbb
jjguillen <jjavierguillen@gmail.com>
2025-11-25 18:28:49 +0100

    Ejercicios cadenas

--- java2026/java2026/src/tema3/cadenas/ejercicios/Ejercicio7.java
+++ java2026/java2026/src/tema3/cadenas/ejercicios/Ejercicio7.java
@@ -12,13 +12,36 @@ public class Ejercicio7 {
          * subcadena = "de"
          * contador = 2
          *
-         * indexOf
+         * indexOf(subcadena, desdeIndice) --> buscar a partir de una posición
+         * -1 si no encuentra la subcadena
+         * indice si encuentra la subcadena
          *
          */
 
-        String cadena = "El día de hoy es el más largo de mi vida.";
+        String cadena = "de día de hoy es el de más largo de mi vida.";
         String subcadena = "de";
         int contador = 0;
+        int fromIndex = -1;
+
+        //System.out.println(cadena.indexOf(subcadena,fromIndex));
+
+        do {
+            //Busque a partir de la posición de la que subcadena que encuentra
+            fromIndex = cadena.indexOf(subcadena,fromIndex+1);
+            if (fromIndex != -1) {
+                contador++;
+            }
+        } while (fromIndex != -1);
+        System.out.println(contador);
+
+        //Otra forma de hacerlo con while donde se asigna la variable y luego se compara
+        contador = 0;
+        fromIndex = -1;
+        while ((fromIndex = cadena.indexOf(subcadena,fromIndex+1)) != -1) {
+            contador++;
+        }
+        System.out.println(contador);
+
 
 
 
