31b909070ea6140b625f79ca441eda092b1b5ec4
jjguillen <jjavierguillen@gmail.com>
2025-11-25 18:09:06 +0100

    Ejercicios cadenas

--- java2026/java2026/src/tema3/cadenas/ejercicios/Ejercicio6.java
+++ java2026/java2026/src/tema3/cadenas/ejercicios/Ejercicio6.java
@@ -9,6 +9,26 @@ public class Ejercicio6 {
          * consonantes y espacios en blanco contiene la cadena introducida.
          */
 
+        String texto = "Javi tengo la cabeza loca ya, para por Dios";
+        int contadorVocales = 0, contadorConsonantes = 0, contadorEspacios = 0;
 
+        for(int i=0; i < texto.length(); i++) {
+            if (texto.charAt(i) == 'a' || texto.charAt(i) == 'e' || texto.charAt(i) == 'i'
+                    || texto.charAt(i) == 'o' || texto.charAt(i) == 'u' || texto.charAt(i) == 'á'
+                    || texto.charAt(i) == 'é' || texto.charAt(i) == 'í' || texto.charAt(i) == 'ó'
+                    || texto.charAt(i) == 'ú') {
+                contadorVocales++;
+            } else if (texto.charAt(i) == ' ') {
+                contadorEspacios++;
+            } else if (texto.charAt(i) == ',' || texto.charAt(i) == '.') {
+                continue;
+            } else {
+                contadorConsonantes++;
+            }
+        }
+
+        System.out.println("Contador vocales: " + contadorVocales);
+        System.out.println("Contador consonantes: " + contadorConsonantes);
+        System.out.println("Contador espacios: " + contadorEspacios);
     }
 }
