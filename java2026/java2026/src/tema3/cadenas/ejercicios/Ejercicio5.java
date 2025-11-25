f2104728fa964725e0ba667a346bec74d74e2516
jjguillen <jjavierguillen@gmail.com>
2025-11-25 17:55:54 +0100

    Ejercicios cadenas

--- java2026/java2026/src/tema3/cadenas/ejercicios/Ejercicio5.java
+++ java2026/java2026/src/tema3/cadenas/ejercicios/Ejercicio5.java
@@ -12,9 +12,30 @@ public class Ejercicio5 {
         String cadena = "Javi tengo la cabeza loca ya, para!!!";
         System.out.println(cadena);
 
+        //Versión con StringBuffer
         StringBuffer sb = new StringBuffer(cadena);
         sb.reverse();
         System.out.println(sb);
+
+        //Versión con String
+        String cadenaInvertida = "";
+        for(int i = cadena.length()-1; i >= 0; i--) {
+            System.out.print(cadena.charAt(i));
+            cadenaInvertida = cadenaInvertida +  cadena.charAt(i); //Ineficiente
+        }
+
+        System.out.println();
+        System.out.println(cadenaInvertida);
+
+        //Versión con String
+        String cadenaInvertida2 = "";
+        for(int i = 0; i < cadena.length(); i++) {
+            cadenaInvertida2 = cadena.charAt(i) + cadenaInvertida2; //Ineficiente
+        }
+
+        System.out.println();
+        System.out.println(cadenaInvertida2);
+
 
 
     }
