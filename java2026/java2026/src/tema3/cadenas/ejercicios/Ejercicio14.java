d4c9faba7ac26fa114505c8ef3b30767c5236501
jjguillen <jjavierguillen@gmail.com>
2025-11-27 17:26:05 +0100

    Ejercicios cadenas

---
+++ java2026/java2026/src/tema3/cadenas/ejercicios/Ejercicio14.java
@@ -0,0 +1,28 @@
+package tema3.cadenas.ejercicios;
+
+public class Ejercicio14 {
+
+    /**
+     * Cifrado César. Crea un algoritmo de encriptación que reciba una frase y
+     * una clave numérica. Sumará en código ASCII esa clave a cada letra de la frase.
+     * Implementa también una función para desencriptar.
+     *
+     * "Esto es un secreto, el examen va a ser complicado"
+     * Clave 3
+     * "Hv..."
+     *
+     * String -> charAt -> char
+     * char <-> int  char + 3 --> sumar al código ASCII 3
+     *
+     */
+
+    public static String encriptar(String frase, int clave) {
+        StringBuffer sb = new StringBuffer();
+
+        return sb.toString(); //StringBuffer lo convierte a String
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
