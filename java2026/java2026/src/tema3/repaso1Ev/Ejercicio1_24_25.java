3e2e84acb8c94d9b2eca4cd2cfd6194cf583349e
jjguillen <jjavierguillen@gmail.com>
2025-12-01 17:07:15 +0100

    Ejercicios repaso

--- java2026/java2026/src/tema3/repaso1Ev/Ejercicio1_24_25.java
+++ java2026/java2026/src/tema3/repaso1Ev/Ejercicio1_24_25.java
@@ -1,5 +1,6 @@
 package tema3.repaso1Ev;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio1_24_25 {
@@ -41,19 +42,74 @@ public class Ejercicio1_24_25 {
         matriz[fila2] = aux;
     }
 
+
+    public static int sumarFila(int[][] matriz, int fila) {
+        //0 1 2 3 4
+        //1 2 3 4 5
+        int suma = 0;
+        //Recorrer la fila y sumar cada elemento
+        for(int j=0; j < matriz[fila].length; j++) {
+            suma += matriz[fila][j];
+        }
+        return suma;
+    }
+
+    public static boolean comprobarDiagonales(int[][] matriz) {
+        boolean iguales = true;
+        //4 5 6 7 8  0,0   0,4
+        //6 3 1 2 3  1,1   1,3
+        //8 5 2 3 1  2,2   2,2
+        //9 6 3 5 4  3,3   3,1
+        //7 4 1 2 5  4,4   4,0
+
+        for(int i=0; i < matriz.length; i++) {
+            for(int j=0; j < matriz[i].length; j++) {
+                //Diagonal principal
+                if (i==j) {
+                    //Si no coinciden, no seguir comprobando, no son iguales
+                    if (matriz[i][j] != matriz[i][matriz.length-1-j]) {
+                        iguales = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return iguales;
+    }
+
+    public static int[] calcularMenor(int[][] matriz) {
+        int[] datos = new int[3]; //0 - valor, 1 - i, 2 - j
+        datos[0] = matriz[0][0];
+        //datos[1] = 0;
+        //datos[2] = 0;
+        for(int i=0; i < matriz.length; i++) {
+            for(int j=0; j < matriz[i].length; j++) {
+                if (matriz[i][j] < datos[0]) {
+                    datos[0] = matriz[i][j];
+                    datos[1] = i;
+                    datos[2] = j;
+                }
+            }
+        }
+
+        return datos;
+    }
+
     public static void main(String[] args) {
 
         //No olvidar hacer 20x20
         int[][] matriz = new int[5][5];
         int opcion = 0;
         int colum1, colum2;
-        int menor, menorI, menorJ;
+        int fila, resultado;
+        int[] menor; // 0 -> el valor menor, 1 -> la fila del menor, 2 -> la columna del menor
+        boolean diagonalesIguales;
         Scanner sc = new Scanner(System.in);
 
         rellenarMatriz(matriz);
-        pintarMatriz(matriz);
 
         do {
+            pintarMatriz(matriz);
             System.out.println("INTRODUCE OPCIÓN:");
             System.out.println("1. Intercambiar columna");
             System.out.println("2. Sumar fila");
@@ -82,12 +138,32 @@ public class Ejercicio1_24_25 {
                     }
                     break;
                 case 2:
+                    try {
+                        System.out.println("Dime la fila a sumar:");
+                        fila = Integer.parseInt(sc.nextLine());
+                        resultado = sumarFila(matriz, fila);
+                        System.out.println("La suma de la fila " + fila + " es " + resultado);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Error: Formato de datos invalido");
+                    }
                     break;
                 case 3:
+                    diagonalesIguales = comprobarDiagonales(matriz);
+                    System.out.println("Las matrices son iguales: " + diagonalesIguales);
                     break;
                 case 4:
+                    menor = calcularMenor(matriz);
+                    System.out.println("El menor valor es: " + menor[0] + " en la fila "
+                            + menor[1] + " y columna " + menor[2]);
                     break;
                 case 5:
+                    /*
+                    //Ordenar filas
+                    for(int i=0; i < matriz.length-1; i++) {
+                        Arrays.sort(matriz[i]);
+                    }
+                     */
+                    ordenarColumnas(matriz);
                     break;
                 case 6:
                     System.out.println("Gracias por usar nuestro programa");
