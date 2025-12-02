6b1a2bb2a08a9cfa98c2862dfd817db08c14e6da
jjguillen <jjavierguillen@gmail.com>
2025-12-02 17:39:33 +0100

    Ejercicios repaso

--- java2026/java2026/src/tema3/repaso1Ev/AhorcadoSimple.java
+++ java2026/java2026/src/tema3/repaso1Ev/AhorcadoSimple.java
@@ -1,6 +1,30 @@
 package tema3.repaso1Ev;
 
+import java.util.Scanner;
+
 public class AhorcadoSimple {
+
+    public static void pintarPalabraOculta(StringBuffer palabra) {
+        for(int i=0; i < palabra.length(); i++) {
+            System.out.print(palabra.charAt(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void rellenarPalabraOculta(StringBuffer sb) {
+        for(int i=0; i < sb.capacity(); i++) {
+            sb.append("_");
+        }
+    }
+
+    public static void sustituirLetra(StringBuffer sb, char letra, String palabra) {
+        for(int i=0; i < sb.length(); i++) {
+            if (palabra.charAt(i) == letra) {
+                sb.setCharAt(i, letra);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         /**
          * Implementa el juego del ahorcado con una palabra predefinida (ej: "JAVA").
@@ -12,7 +36,50 @@ public class AhorcadoSimple {
          * Gana si adivina la palabra antes de quedarse sin vidas.
          */
 
+        Scanner teclado = new Scanner(System.in);
 
+        int intentos = 6;
+        char letra;
+
+        String[] palabras = {"amigo", "gallina", "huevo", "perro", "ladrillo"};
+        int aleatorio = (int) (Math.random() * palabras.length);
+        //System.out.println(palabras[aleatorio]);
+        String palabraAdivinar = palabras[aleatorio];
+        StringBuffer palabraOculta = new StringBuffer(palabraAdivinar.length());
+
+        //Llenamos la palabra de guiones
+        rellenarPalabraOculta(palabraOculta);
+
+        while(intentos > 0) {
+            //Pintamos la palabra actual
+            pintarPalabraOculta(palabraOculta);
+
+            System.out.println("Dime una letra:");
+            letra = teclado.next().charAt(0);
+
+            //Buscar la letra en la palabra
+            if (palabraAdivinar.indexOf(letra) != -1) {
+                sustituirLetra(palabraOculta, letra, palabraAdivinar);
+            } else {
+                //No encontrada
+                System.out.println("Letra no encontrada");
+                intentos--;
+            }
+
+            //Comprobar fin del juego
+            if (palabraOculta.toString().equals(palabraAdivinar)) {
+                System.out.println("GANASTE --> " + palabraAdivinar);
+                break;
+            }
+
+            System.out.println("Intentos restantes: " + intentos);
+        }
+
+        if (intentos == 0) {
+            System.out.println("PERDISTE");
+        }
+
+        teclado.close();
 
     }
 }
