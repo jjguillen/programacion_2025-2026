b47780dca0a6cb9d4960f182555d48c4779fe9ca
jjguillen <jjavierguillen@gmail.com>
2025-12-02 17:16:28 +0100

    Ejercicios repaso

---
+++ java2026/java2026/src/tema3/repaso1Ev/AhorcadoSimple.java
@@ -0,0 +1,18 @@
+package tema3.repaso1Ev;
+
+public class AhorcadoSimple {
+    public static void main(String[] args) {
+        /**
+         * Implementa el juego del ahorcado con una palabra predefinida (ej: "JAVA").
+         * Muestra al inicio una serie de guiones (_) del tamaño de la palabra.
+         * El jugador ingresa letras de una en una.
+         * Si la letra está en la palabra, revela su posición (o posiciones) reemplazando el guión.
+         * Si no, resta una vida (comienza con 6 vidas).
+         * Usa un array de char para la palabra oculta y otro para la palabra con pistas.
+         * Gana si adivina la palabra antes de quedarse sin vidas.
+         */
+
+
+
+    }
+}
