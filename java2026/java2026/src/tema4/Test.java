8071cc126680bd9332367c09634706480f11397a
jjguillen <jjavierguillen@gmail.com>
2025-12-16 17:11:26 +0100

    Examen 1EV

---
+++ java2026/java2026/src/tema4/Test.java
@@ -0,0 +1,26 @@
+package tema4;
+
+public class Test {
+
+    public static void main(String[] args) {
+        /**
+         * 1. Crea un clase Personaje
+         * 2. Debe tener las siguientes propiedades (privadas):
+         * - nombre: String
+         * - clase: String
+         * - salud: int o Integer
+         * - mana: int o Integer
+         * 3. Crea un constructor que inicialice todas las propiedades
+         * 4. Crea los métodos getters y setters para todas las propiedades*
+         * 5. Crea el toString para que pinte todas las propiedades (StringBuffer)
+         * 6. Crea un método void quitarSalud(int vida) que reste esa vida a la del personaje
+         * 7. Crea un método void revivir() que ponga la vida a 100.
+         * 8. Luego en el main de esta clase crea dos personajes diferentes pers1 y pers2
+         * 9. Quítale vida a uno de ellos
+         * 10. Pinta los dos personajes
+         *
+         */
+
+
+    }
+}
