02b754e0e92743a0ced940b8c292a3cc41fab694
jjguillen <jjavierguillen@gmail.com>
2025-12-16 17:13:03 +0100

    Examen 1EV

--- java2026/java2026/src/tema4/Test.java
+++ java2026/java2026/src/tema4/Test.java
@@ -11,12 +11,12 @@ public class Test {
          * - salud: int o Integer
          * - mana: int o Integer
          * 3. Crea un constructor que inicialice todas las propiedades
-         * 4. Crea los métodos getters y setters para todas las propiedades*
+         * 4. Crea los métodos getters y setters para todas las propiedades
          * 5. Crea el toString para que pinte todas las propiedades (StringBuffer)
          * 6. Crea un método void quitarSalud(int vida) que reste esa vida a la del personaje
          * 7. Crea un método void revivir() que ponga la vida a 100.
          * 8. Luego en el main de esta clase crea dos personajes diferentes pers1 y pers2
-         * 9. Quítale vida a uno de ellos
+         * 9. Quítale vida a uno de ellos, dejar uno sin vida, revivirlo
          * 10. Pinta los dos personajes
          *
          */
