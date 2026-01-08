37521dd3003692137fe82596d04fba67e1a041d1
jjguillen <jjavierguillen@gmail.com>
2026-01-08 17:01:45 +0100

    Ejemplos POO

---
+++ java2026/java2026/src/tema4/Medico.java
@@ -0,0 +1,27 @@
+package tema4;
+
+public class Medico {
+
+    /**
+     * Crear clase Medico
+     * Propiedades:
+     * - nombre
+     * - especialidad (enum Especialidad ->
+     *     GENERAL, DIGESTIVO, OTORRINO, CARDIOLOGIA, NEUROLOGIA, TRAUMATOLOGIA)
+     * - edad
+     * - email
+     * - telefono
+     * - estado (enum Estado -> ACTIVO, INACTIVO)
+     *
+     * Constructor con todo menos 'estado'. El estado por defecto es ACTIVO.
+     * Getters de todo
+     * Setters de todo menos 'estado'
+     * ToString
+     * Metodos propios:
+     * - baja() -> pone el estado en INACTIVO
+     * - alta() -> pone el estado en ACTIVO
+     *
+     * En un main crea dos médicos, pon uno de baja y pinta los dos
+     */
+
+}
