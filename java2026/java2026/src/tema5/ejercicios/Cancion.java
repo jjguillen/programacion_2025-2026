82fc8878a4b3aa41a9be2109957524e403245530
jjguillen <jjavierguillen@gmail.com>
2026-01-13 18:29:36 +0100

    Ejemplos POO - Constructor copia y equals

---
+++ java2026/java2026/src/tema5/ejercicios/Cancion.java
@@ -0,0 +1,15 @@
+package tema5.ejercicios;
+
+public class Cancion {
+
+    private String titulo;
+    private String artista;
+    private Integer duracion;
+
+    //Constructor parametrizado
+    //Constructor copia
+    //Getters y setters
+    //ToString
+    //Equals con titulo y artista
+
+}
