ef3f5e9e08a985b782074c1a66de28fd0b328a03
jjguillen <jjavierguillen@gmail.com>
2026-01-13 18:32:14 +0100

    Ejemplos POO - Constructor copia y equals

---
+++ java2026/java2026/src/tema5/ejercicios/ListaSpotify.java
@@ -0,0 +1,17 @@
+package tema5.ejercicios;
+
+import java.util.ArrayList;
+
+public class ListaSpotify {
+
+    private String nombre;
+    private ArrayList<Cancion> canciones;
+
+    //Constructor parametrizado
+    //Constructor copia
+    //Getters y setters
+    //ToString
+    //Añadir canción
+    //Eliminar canción
+
+}
---
+++ java2026/java2026/src/tema5/ejercicios/Test.java
@@ -0,0 +1,17 @@
+package tema5.ejercicios;
+
+public class Test {
+    public static void main(String[] args) {
+
+        //1. Crea una lista de Spotify de 10 canciones
+        //2. Píntala
+        //3. Recorre la lista con un for y pinta cada canción su título y autor solo
+        //4. Pide por teclado un título y un autor
+        //5. Elimina la canción de la lista con ese título y ese autor
+
+
+
+
+
+    }
+}
