b3815674a119640792b8b86f412bca56c4943518
jjguillen <jjavierguillen@gmail.com>
2026-01-22 17:27:17 +0100

    Novedades Java25

--- java2026/java2026/src/tema5/ejercicioclase/modelos/TareaBase.java
+++ java2026/java2026/src/tema5/ejercicioclase/modelos/TareaBase.java
@@ -1,4 +1,37 @@
 package tema5.ejercicioclase.modelos;
 
-public class TareaBase {
+import java.time.LocalDate;
+
+/**
+ * Clase abstracta (no se pueden crear objetos de esta clase)
+ * Va a ser el padre de una herencia --> TareaUrgente y TareaProgramada serán sus hijas
+ * Implementa interfaz tarea
+ */
+public abstract class TareaBase {
+
+    private static Integer contadorId = 0;
+
+    private Integer id;
+    private String titulo;
+    private String descripcion;
+    private PrioridadTarea prioridad;
+    private EstadoTarea estado;
+    private LocalDate fechaCreacion;
+    private LocalDate fechaCompletada;
+
+    public TareaBase(String titulo, String descripcion, PrioridadTarea prioridad) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+    }
+
+
+
+
+
+
+
+
+
+
 }
