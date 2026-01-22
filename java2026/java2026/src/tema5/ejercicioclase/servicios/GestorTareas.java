23f2d238c203ad245727edc5ba39116b691484a3
jjguillen <jjavierguillen@gmail.com>
2026-01-22 18:51:25 +0100

    Novedades Java25

--- java2026/java2026/src/tema5/ejercicioclase/servicios/GestorTareas.java
+++ java2026/java2026/src/tema5/ejercicioclase/servicios/GestorTareas.java
@@ -1,6 +1,7 @@
 package tema5.ejercicioclase.servicios;
 
 import tema5.ejercicioclase.modelos.EstadoTarea;
+import tema5.ejercicioclase.modelos.PrioridadTarea;
 import tema5.ejercicioclase.modelos.Tarea;
 import tema5.ejercicioclase.modelos.TareaBase;
 
@@ -51,7 +52,7 @@ public class GestorTareas {
      * @param estado de la tarea
      * @return ArrayList<Tarea>
      */
-    public ArrayList<Tarea> buscarTareasPorEstado(EstadoTarea estado) {
+    public ArrayList<Tarea> getTareasPorEstado(EstadoTarea estado) {
         ArrayList<Tarea> lista = new ArrayList<>();
         for(Tarea tarea : tareas) {
             if ( tarea.getEstado().equals(estado) ) {
@@ -60,6 +61,45 @@ public class GestorTareas {
             }
         }
         return lista;
+    }
+
+    /**
+     * Devolver una lista con todas las tareas con una prioridad determinada
+     * @param prioridad de la tarea
+     * @return lista con las tareas con la misma prioridad
+     */
+    public ArrayList<Tarea> getTareasPorPrioridad(PrioridadTarea prioridad) {
+        ArrayList<Tarea> lista = new ArrayList<>();
+        for(Tarea tarea : tareas) {
+            if ( tarea.getPrioridad().equals(prioridad) ) {
+                //Añadir a la nueva lista si la tarea tiene la misma prioridad
+                lista.add(tarea);
+            }
+        }
+        return lista;
+    }
+
+    /**
+     * Devolver una lista con todas las tareas URGENTES (TareaUrgente)
+     * @return ArrayList<Tarea>
+     */
+    public ArrayList<Tarea> getTareasUrgentes() {
+        ArrayList<Tarea> lista = new ArrayList<>();
+        for(Tarea tarea : tareas) {
+            if ( tarea.getTipo().equals("URGENTE") ) {
+                //Añadir a la nueva lista si la tarea es URGENTE
+                lista.add(tarea);
+            }
+        }
+        return lista;
+    }
+
+    /**
+     * Devolver una lista con todas las tareas PENDIENTES
+     * @return ArrayList<Tarea>
+     */
+    public ArrayList<Tarea> getTareasPendientes() {
+        return getTareasPorEstado(EstadoTarea.PENDIENTE);
     }
 
 
