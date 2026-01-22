e4ce73dd361359877c2221df84af7d6832f12e43
jjguillen <jjavierguillen@gmail.com>
2026-01-22 18:03:53 +0100

    Novedades Java25

--- java2026/java2026/src/tema5/ejercicioclase/modelos/TareaUrgente.java
+++ java2026/java2026/src/tema5/ejercicioclase/modelos/TareaUrgente.java
@@ -1,5 +1,9 @@
 package tema5.ejercicioclase.modelos;
 
+import tema4.Estado;
+
+import java.time.LocalDateTime;
+
 public class TareaUrgente extends TareaBase {
 
     private Integer plazoHoras;
@@ -20,5 +24,43 @@ public class TareaUrgente extends TareaBase {
     @Override
     public String getTipo() {
         return "URGENTE";
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("TareaUrgente{");
+        sb.append("id=").append(id);
+        sb.append(", titulo='").append(titulo).append('\'');
+        sb.append(", descripcion='").append(descripcion).append('\'');
+        sb.append(", prioridad=").append(prioridad);
+        sb.append(", estado=").append(estado);
+        sb.append(", fechaCreacion=").append(fechaCreacion);
+        sb.append(", fechaCompletada=").append(fechaCompletada);
+        sb.append(", plazoHoras=").append(plazoHoras);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public boolean estaVencida() {
+        /*
+        if (this.estado != EstadoTarea.COMPLETADA && this.plazoHoras < 24) {
+            return false;
+        } else {
+            return true;
+        }
+        */
+
+        //Si la fechaCreación + plazoHoras < fechaHoraActual
+
+        //Sacamos la fecha y hora actual
+        LocalDateTime fechaActual = LocalDateTime.now();
+
+        //Convertimos la fecha de creación a LocalDateTime
+        LocalDateTime fechaCreacionLDT = fechaCreacion.atTime(0,0);
+        fechaCreacionLDT = fechaCreacionLDT.plusHours(plazoHoras);
+
+        //Comparamos las dos fechas
+        return fechaActual.isAfter(fechaCreacionLDT);
+
     }
 }
