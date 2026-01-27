e58d9fddafa417f0450d93fd8871b10d7eac0ed6
jjguillen <jjavierguillen@gmail.com>
2026-01-27 16:55:24 +0100

    Novedades Java25

--- java2026/java2026/src/tema5/ejercicioclase2/modelos/ContenidoDigital.java
+++ java2026/java2026/src/tema5/ejercicioclase2/modelos/ContenidoDigital.java
@@ -1,5 +1,7 @@
 package tema5.ejercicioclase2.modelos;
 
+import java.util.UUID;
+
 public abstract class ContenidoDigital {
 
     protected String id;
@@ -8,6 +10,19 @@ public abstract class ContenidoDigital {
     protected Integer year;
     protected Integer size; //MegaBytes
 
+    public ContenidoDigital(String title, String author, Integer year, Integer size) {
+        this.id = UUID.randomUUID().toString();
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.size = size;
+    }
 
-
+    public ContenidoDigital(ContenidoDigital otro) {
+        this.id = otro.id;
+        this.title = otro.title;
+        this.author = otro.author;
+        this.year = otro.year;
+        this.size = otro.size;
+    }
 }
