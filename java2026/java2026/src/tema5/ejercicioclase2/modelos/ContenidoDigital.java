312bd98805eba0f01d2b0ddf53d9b288cc443067
jjguillen <jjavierguillen@gmail.com>
2026-01-27 16:53:26 +0100

    Novedades Java25

---
+++ java2026/java2026/src/tema5/ejercicioclase2/modelos/ContenidoDigital.java
@@ -0,0 +1,13 @@
+package tema5.ejercicioclase2.modelos;
+
+public abstract class ContenidoDigital {
+
+    protected String id;
+    protected String title;
+    protected String author;
+    protected Integer year;
+    protected Integer size; //MegaBytes
+
+
+
+}
