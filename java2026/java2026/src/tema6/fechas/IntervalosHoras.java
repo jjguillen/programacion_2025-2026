568324f1a170123495c05f12f5d54a2c8ac5dbf6
jjguillen <jjavierguillen@gmail.com>
2026-02-12 17:02:09 +0100

    Fechas y horas

--- java2026/java2026/src/tema6/fechas/IntervalosHoras.java
+++ java2026/java2026/src/tema6/fechas/IntervalosHoras.java
@@ -2,9 +2,10 @@ package tema6.fechas;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
-public class Intervalos {
+public class IntervalosHoras {
     static void main() {
 
         Instant instante = Instant.ofEpochSecond(1000000000l);
@@ -15,12 +16,10 @@ public class Intervalos {
 
         //Calcula el tiempo transcurrido al ejecutar un código
         Instant a1 = Instant.now();
-
         ArrayList<Long> numeros = new ArrayList<>();
         for(long i=0; i < 100000000L; i++) {
             numeros.add(i);
         }
-
         Instant a2 = Instant.now();
 
         //Para conocer el tiempo transcurrido entre horas hay dos formas:
@@ -32,6 +31,24 @@ public class Intervalos {
         //2. Usando la clase Duration
         Duration duracion = Duration.between(a1, a2);
         IO.println(duracion);
+
+        //Ejemplo de duración con más de una hora
+        LocalTime h1 = LocalTime.now();
+        LocalTime h2 = LocalTime.now()
+                .plusHours(10)
+                .plusMinutes(10);
+
+        Duration duracionHora = Duration.between(h1, h2);
+        IO.println(duracionHora);
+        IO.println(duracionHora.toHours()); //El total de horas de la duración
+        IO.println(duracionHora.toMinutes()); //El total de minutos de la duración
+
+
+
+
+
+
+
 
 
     }
