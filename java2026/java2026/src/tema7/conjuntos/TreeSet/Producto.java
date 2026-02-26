87f0d22c92bbdc9d60a111b24f6fab7e4b617532
jjguillen <jjavierguillen@gmail.com>
2026-02-26 17:24:55 +0100

    COLECCIONES: Set, HashSet, TreeSet

--- java2026/java2026/src/tema7/conjuntos/TreeSet/Producto.java
+++ java2026/java2026/src/tema7/conjuntos/TreeSet/Producto.java
@@ -1,12 +1,76 @@
 package tema7.conjuntos.TreeSet;
 
+import java.util.Objects;
+
 public class Producto {
 
+    private static long contador = 1000;
+
+    private final long id = contador++;
     private String nombre;
     private String categoria;
     private String descripcion;
     private Double precio;
 
+    public Producto(String nombre, String categoria, String descripcion, Double precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Producto{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", categoria='").append(categoria).append('\'');
+        sb.append(", descripcion='").append(descripcion).append('\'');
+        sb.append(", precio=").append(precio);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return id == producto.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
