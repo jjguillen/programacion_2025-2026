639e509ce024352eb0125adab4caca6dd0eb55cd
jjguillen <jjavierguillen@gmail.com>
2026-03-17 16:46:51 +0100

    Maps

--- java2026/java2026/src/tema7/mapas/Ejemplo.java
+++ java2026/java2026/src/tema7/mapas/Ejemplo.java
@@ -2,6 +2,8 @@ package tema7.mapas;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Ejemplo {
 
@@ -27,7 +29,35 @@ public class Ejemplo {
         //Obtener valor por clave
         IO.println(diccionario.get("house"));
 
+        //Preguntar si existe una clave
+        IO.println(diccionario.containsKey("house"));
+        IO.println(diccionario.containsKey("dog"));
 
+        //Recorrer el mapa con for -> map.keySet() nos devuelve un Set con las claves
+        //Para cada clave sacamos su valor con diccionario.get(clave)
+        IO.println("--- Claves ---");
+        for(String clave : diccionario.keySet()) {
+            IO.println(clave + " -> " + diccionario.get(clave));
+        }
+
+        //Mostrar todos los valores
+        IO.println("--- Valores ---");
+        for(String valor : diccionario.values()) {
+            IO.println(valor);
+        }
+
+        //Mostrar los pares clave-valor como Entry<Clave, Valor>
+        IO.println("--- Entries ---");
+        for(Map.Entry<String,String> entrada : diccionario.entrySet()) {
+            IO.println(entrada.getKey() + " -> " + entrada.getValue());
+        }
+
+        //Pintar el diccionario ordenado por claves
+        IO.println("--- Ordenado por claves ---");
+        Set<String> clavesOrdenadas = new TreeSet<>(diccionario.keySet());
+        for(String clave : clavesOrdenadas) {
+            IO.println(clave + " -> " + diccionario.get(clave));
+        }
 
 
 
